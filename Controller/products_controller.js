0fddeba55a2c13b152c2231d3f2530a21534b742
Rahul Singh <Rahulsingh4533@gmail.com>
2023-07-29 22:03:36 +0530

    Finish

--- Controller/products_controller.js
+++ Controller/products_controller.js
@@ -7,7 +7,7 @@ module.exports.create = function (req, res) {
 
   res.json({
     data: {
-      Product
+      Product,
     },
   });
 };
@@ -25,59 +25,44 @@ module.exports.products = async (req, res) => {
 
 //  delete the record by id
 
-module.exports.delete = function(req, res) {
+module.exports.delete = function (req, res) {
+  console.log(req.params.productID);
 
-   
-    console.log(req.params.productID);
- 
-
-Products.deleteOne( {_id: req.params.productID})
-
+  Products.deleteOne({ _id: req.params.productID });
 
   res.json({
     data: {
       message: "product deleted",
     },
-})
-    
- 
+  });
 };
-
-
 
 // update the qunatity
 
-
-module.exports.update=async function(req,res){
-
-  const ID= req.params.id;
+module.exports.update = async function (req, res) {
+  const ID = req.params.id;
 
   try {
-    const result= await Products.findById(ID);
+    const result = await Products.findById(ID);
 
     //Now getting the number and add the to the exist quantity into new variable
 
-    const newquantity= parseInt(result.quantity)+ parseInt(req.query.number)
-   
+    const newquantity = parseInt(result.quantity) + parseInt(req.query.number);
+
     // Now Find the Document by id and Update the new value init
 
-    const updatevalue= await Products.findByIdAndUpdate(Id.{
-      quantity: newquantity
-    })
+    const updatevalue = await Products.findByIdAndUpdate(result.id, {
+      quantity: newquantity,
+    });
 
     res.json({
-      data:{
-        updatevalue
-      }
-    })
-
-
+      data: {
+        updatevalue,
+      },
+    });
   } catch (error) {
-    
     res.json({
-      message: "Error While Find The data "
-      
-    })
+      message: "Error While Find The data ",
+    });
   }
-
-}
+};
